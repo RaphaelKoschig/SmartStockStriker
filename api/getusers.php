65fc76abd385055d9ec3230a5be72d3d76d67b2d
Raphael Koschig <raphael.koschig@gmail.com>
2020-02-20 16:37:26 +0100

    MAJ 20/02/2020 16h37

--- api/getusers.php
+++ api/getusers.php
@@ -6,14 +6,14 @@ header('Content-Type: application/json');
  * connexion à la base de données
  */
 try {
-    $connexion = new PDO('mysql:host=db5000296647.hosting-data.io;dbname=dbs289874','dbu136770','JD90200jd*');
+    $connexion = new PDO('mysql:host=db5000303592.hosting-data.io;dbname=dbs296581','dbu526227','raph1188SSSDB!:;,');
     $retour["success"] = true;
 }
 catch(Exception $ex) {
     $retour["success"] = false;
 }
 
-$request = $connexion->prepare("SELECT * FROM users");
+$request = $connexion->prepare("SELECT * FROM user");
 $request->execute();
 
 $retour = $request->fetchAll(PDO::FETCH_ASSOC);
